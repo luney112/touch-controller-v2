d55f4ad6d564c40fd0d3acb23a7bfc9c3cf94cdd
Jeremy <jeremyjsimpson@outlook.com>
2025-05-18 14:21:13 -0500

    code test

--- firmware/touch-controller/src/air.cpp
+++ firmware/touch-controller/src/air.cpp
@@ -1,3 +1,9 @@
+/**
+ * Some good reference material:
+ *  https://www.st.com/resource/en/user_manual/um3038-a-guide-to-using-the-vl53l7cx-timeofflight-multizone-ranging-sensor-with-90-fov-stmicroelectronics.pdf
+ *  https://www.st.com/resource/en/datasheet/vl53l7cx.pdf
+ *  https://community.st.com/t5/imaging-sensors/vl53l8cx-example-quot-platform-h-quot-ram-reduction/td-p/714296
+ */
 #include "air.h"
 #include "layout.h"
 #include "serial.h"
@@ -38,9 +44,9 @@ bool AirController::init() {
 
 void AirController::initSingleSensor(VL53L7CX *sensor, uint8_t addr) {
   assertOk(sensor->init_sensor(addr), "init_sensor");
-  // assertOk(sensor->vl53l7cx_set_ranging_frequency_hz(30), "vl53l7cx_set_ranging_frequency_hz");
+  assertOk(sensor->vl53l7cx_set_ranging_frequency_hz(60), "vl53l7cx_set_ranging_frequency_hz");
   assertOk(sensor->vl53l7cx_set_ranging_mode(VL53L7CX_RANGING_MODE_CONTINUOUS), "vl53l7cx_set_ranging_mode");
-  assertOk(sensor->vl53l7cx_set_sharpener_percent(60), "vl53l7cx_set_sharpener_percent");
+  // assertOk(sensor->vl53l7cx_set_sharpener_percent(40), "vl53l7cx_set_sharpener_percent");
   assertOk(sensor->vl53l7cx_start_ranging(), "vl53l7cx_start_ranging");
 }
 
@@ -53,24 +59,30 @@ void AirController::assertOk(uint8_t status, const char *name) {
 uint8_t AirController::getBlockedSensors() {
   VL53L7CX_ResultsData resultsData;
 
-  if (interruptCount > 0) {
-    interruptCount = 0;
-    assertOk(sensorRight->vl53l7cx_get_ranging_data(&resultsData), "vl53l7cx_get_ranging_data");
+  // if (interruptCount > 0) {
+  //   interruptCount = 0;
+  //   assertOk(sensorRight->vl53l7cx_get_ranging_data(&resultsData), "vl53l7cx_get_ranging_data");
 
-    int32_t sum = 0;
-    int32_t cnt = 0;
-    for (int i = 0; i < SpadIndexCount; i++) {
-      uint8_t idx = SpadIndexes[i];
-      if (resultsData.target_status[idx] == 5) {
-        sum += resultsData.distance_mm[idx];
-        cnt++;
-      }
-    }
-    if (cnt > 0) {
-      double distance = static_cast<double>(sum) / cnt;
-      serial->writeDebugLogf("%fmm", distance);
-    }
-  }
+  //   int32_t sum = 0;
+  //   int32_t cnt = 0;
+  //   for (int i = 0; i < SpadIndexCount; i++) {
+  //     uint8_t idx = SpadIndexes[i];
+  //     uint8_t status = resultsData.target_status[idx];
+  //     uint8_t detected = resultsData.nb_target_detected[idx];
+  //     int16_t distance = resultsData.distance_mm[idx];
+  //     if (status == 5 && detected > 0) {
+  //       sum += distance;
+  //       cnt++;
+  //       if (distance <= 1000) {
+  //         serial->writeDebugLogf("target at %d cm", distance / 10);
+  //       }
+  //     }
+  //   }
+  //   // if (cnt > 0) {
+  //   //   double distance = static_cast<double>(sum) / cnt;
+  //   //   serial->writeDebugLogf("%fmm", distance);
+  //   // }
+  // }
 
   return 0; // TODO: Implement
 }
